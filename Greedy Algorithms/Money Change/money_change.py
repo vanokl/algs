7c95a4cd36812f2b52d46843f267c36140fb68e2
vanokl <vanokl@bk.ru>
2020-02-11 12:48:24 +0300

    greedy algs

--- Greedy Algorithms/Money Change/money_change.py
+++ Greedy Algorithms/Money Change/money_change.py
@@ -3,7 +3,13 @@
 
 def money_change(money):
     assert 0 <= money <= 10 ** 3
-    type here
+    coins = [10, 5, 1]
+    coins_list = []
+    for coin in coins:
+        num_of_coins, money = divmod(money, coin)
+        coins_list.append(num_of_coins)
+    return sum(coins_list)
+
 
 
 if __name__ == '__main__':
